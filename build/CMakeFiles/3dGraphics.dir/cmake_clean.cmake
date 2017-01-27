79e9d2088d8d0fbaf317215fcad75c8b165f8821
Sadman Kazi <sadmansk@protonmail.com>
2017-01-27 18:55:19 -0500

    Add CMake build support

---
+++ build/CMakeFiles/3dGraphics.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3dGraphics.dir/src/display.cpp.o"
+  "CMakeFiles/3dGraphics.dir/src/main.cpp.o"
+  "CMakeFiles/3dGraphics.dir/src/mesh.cpp.o"
+  "CMakeFiles/3dGraphics.dir/src/obj_loader.cpp.o"
+  "CMakeFiles/3dGraphics.dir/src/shader.cpp.o"
+  "CMakeFiles/3dGraphics.dir/src/texture.cpp.o"
+  "CMakeFiles/3dGraphics.dir/src/stb_image.c.o"
+  "3dGraphics.pdb"
+  "3dGraphics"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/3dGraphics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
